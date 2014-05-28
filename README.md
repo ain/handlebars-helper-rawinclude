d0c399dc0fb1e3583583e2dc113be278a184cabf
Ain Tohvri <ain@flashbit.net>
2014-05-28 12:15:06 +0200

    Add initial simplistic version

---
+++ README.md
@@ -0,0 +1,7 @@
+# {{rawinclude}}
+
+> Handlebars helper, includes raw content of the file pointed. Like
+> `handlebars-helper-include` but without context.
+
+## License
+Copyright (c) 2014 Ain Tohvri. Licensed under the GPL license.
---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * Handlebars Helper: {{rawinclude}}
+ * Copyright (c) 2014 Ain Tohvri
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+// node_modules
+var minimatch = require('minimatch');
+var matter = require('gray-matter');
+var file = require('fs-utils');
+var _ = require('lodash');
+
+module.exports.register = function (Handlebars, options, params) {
+
+  var assemble = params.assemble;
+  var grunt = params.grunt;
+  var opts = options || {};
+
+  /**
+   * {{rawinclude}}
+   * Like {{ include }} but without context.
+   *
+   * @param  {String} path    Path of the file to include.
+   * @return {String}         Returns raw content of the file at path.
+   * @example: {{rawinclude '/path/to/compund.svg'}}
+   * @todo support for Array input, minimatch.
+   */
+  Handlebars.registerHelper('rawinclude', function(name) {
+    var result = matter.read(name);
+    return new Handlebars.SafeString(result.content);
+  });
+};
